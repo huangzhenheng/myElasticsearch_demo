4a41d9c027c12cfb8c27a2a8a507570aa130eeda
huangzhenheng <huangzhenheng100@163.com>
2018-02-13 11:30:17 +0800

    修改mq的监听

--- src/main/java/com/hzh/mq/EsConverter.java
+++ src/main/java/com/hzh/mq/EsConverter.java
@@ -5,14 +5,24 @@ import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 import org.springframework.jms.support.converter.MessageConversionException;
 import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
+import com.hzh.dao.UserMapper;
 import com.hzh.index.User;
 
+@Component
 public class EsConverter implements MessageConverter {
-
+	@Autowired
+	private ElasticsearchTemplate template;
+	@Autowired
+	private UserMapper userMapper;
 	@Override
 	public Message toMessage(Object object, Session session)
 			throws JMSException, MessageConversionException {
@@ -26,10 +36,22 @@ public class EsConverter implements MessageConverter {
 		if (message instanceof TextMessage) {
 			TextMessage textMessage = (TextMessage) message;
 			user = new Gson().fromJson(textMessage.getText(), User.class);
+			if (user != null && user.getId() != null) {
+				indexUserByaddOrUpdate(user);
+			}
 		} else {
 			System.err.println("非TextMessage");
 		}
 		return user;
 
+	}
+
+	private void indexUserByaddOrUpdate(User user) {
+		user = userMapper.findUserByid(Long.valueOf(user.getId()));
+		template.index(buildIndex(user));
+	}
+
+	private IndexQuery buildIndex(User user) {
+		return new IndexQueryBuilder().withId(user.getId()).withObject(user).build();
 	}
 }
--- src/main/java/com/hzh/mq/MyQueueListener.java
+++ src/main/java/com/hzh/mq/MyQueueListener.java
@@ -3,55 +3,29 @@ package com.hzh.mq;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.jms.TextMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 import org.springframework.stereotype.Component;
-
-import com.google.gson.Gson;
-import com.hzh.dao.UserMapper;
-import com.hzh.index.User;
 
 /**
  * Created by 123 on 2017/12/27.
  */
 @Component
 public class MyQueueListener implements MessageListener {
+
 	@Autowired
-	private ElasticsearchTemplate template;
-	@Autowired
-	private UserMapper userMapper;
+	private EsConverter esConverter;
 
     @Override
     public void onMessage(Message message) {
 
-		if (message instanceof TextMessage) {
-			TextMessage textMessage = (TextMessage) message;
-			try {
-
-				User user = new Gson().fromJson(textMessage.getText(), User.class);
-				if (user != null && user.getId() != null) {
-					indexUserByaddOrUpdate(user);
-				}
-
-			} catch (JMSException e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.err.println("ssdfgh");
+		try {
+			esConverter.fromMessage(message);
+		} catch (JMSException e) {
+			e.printStackTrace();
 		}
 
     }
 
-	private void indexUserByaddOrUpdate(User user) {
-		user = userMapper.findUserByid(Long.valueOf(user.getId()));
-		template.index(buildIndex(user));
-	}
 
-	private IndexQuery buildIndex(User user) {
-		return new IndexQueryBuilder().withId(user.getId()).withObject(user).build();
-	}
 }
